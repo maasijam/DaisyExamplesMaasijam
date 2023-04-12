fb31087959b49f83b76df5b589ee6e7dc955dd94
maasijam <50107149+maasijam@users.noreply.github.com>
2023-04-12 07:08:11 +0200

    update daisy hank hardware class

--- hank/daisy_hank.cpp
+++ hank/daisy_hank.cpp
@@ -9,48 +9,29 @@ using namespace daisy;
 // HANK Pins
 #define PIN_POT1 15
 #define PIN_POT2 16
+#define PIN_POT3 17
+#define PIN_POT4 18
 
-#define PIN_CV1 17
-#define PIN_CV2 18
+#define PIN_CV1 19
+
 
 
 #define PIN_S1 0
-#define PIN_SW1A 1
-#define PIN_SW1B 2
 
-#define PIN_GATE_OUT 3
-#define PIN_TRIG_IN 4
+#define PIN_TRIG_IN 1
 
-#define PIN_LED1 5
-#define PIN_LED2 6
-#define PIN_LED3 11
-#define PIN_LED4 12
-#define PIN_LED5 13
-#define PIN_LED6 14
-#define PIN_LED7 25
-#define PIN_LED8 26
-#define PIN_LED9 27
-#define PIN_LED10 28
-
-#define PIN_OLED_CS_EXT 7
-#define PIN_OLED_SCK_EXT 8
-#define PIN_OLED_CMD_EXT 9
-#define PIN_OLED_DATA_EXT 10
-
-#define PIN_ENC_A_EXT 19
-#define PIN_ENC_B_EXT 20
-#define PIN_ENC_CLICK_EXT 21
-
-#define PIN_POT3_EXT 24
-#define PIN_CV3_EXT 22
-
-
-
-
-
-
-
-
+#define PIN_RGB1_R 2
+#define PIN_RGB1_G 3
+#define PIN_RGB1_B 4
+#define PIN_RGB2_R 5
+#define PIN_RGB2_G 6
+#define PIN_RGB2_B 7
+#define PIN_RGB3_R 8
+#define PIN_RGB3_G 9
+#define PIN_RGB3_B 10
+#define PIN_RGB4_R 11
+#define PIN_RGB4_G 12
+#define PIN_RGB4_B 13
 
 
 
@@ -60,10 +41,11 @@ void DaisyHank::Init(bool boost)
     seed.Init(boost);
     //seed.SetAudioBlockSize(48);
 
-    sw.Init(seed.GetPin(PIN_SW1A), seed.GetPin(PIN_SW1B));
     s1.Init(seed.GetPin(PIN_S1));
 
-   
+   uint8_t ledr_pin[]    = {PIN_RGB1_R, PIN_RGB2_R, PIN_RGB3_R, PIN_RGB4_R};
+    uint8_t ledg_pin[]    = {PIN_RGB1_G, PIN_RGB2_G, PIN_RGB3_G, PIN_RGB4_G};
+    uint8_t ledb_pin[]    = {PIN_RGB1_B, PIN_RGB2_B, PIN_RGB3_B, PIN_RGB4_B};
 
     // gate in 
     dsy_gpio_pin gateIn1_gpio = seed.GetPin(PIN_TRIG_IN);
@@ -71,62 +53,35 @@ void DaisyHank::Init(bool boost)
 
 
     // ADCs
-    AdcChannelConfig adc_cfg[4];
+    AdcChannelConfig adc_cfg[5];
 
     // POT/CV MUX
     adc_cfg[0].InitSingle(seed.GetPin(PIN_POT1));
     adc_cfg[1].InitSingle(seed.GetPin(PIN_POT2));
-    adc_cfg[2].InitSingle(seed.GetPin(PIN_CV1));
-    adc_cfg[3].InitSingle(seed.GetPin(PIN_CV2));
+    adc_cfg[2].InitSingle(seed.GetPin(PIN_POT3));
+    adc_cfg[3].InitSingle(seed.GetPin(PIN_POT4));
+    adc_cfg[4].InitSingle(seed.GetPin(PIN_CV1));
     
-    seed.adc.Init(adc_cfg,4,daisy::AdcHandle::OVS_128);
+    seed.adc.Init(adc_cfg,5);
  
     //seed.adc.Start();
 
     for(size_t i = 0; i < (KNOB_LAST + CV_LAST); i++) {
-        if(i < 2) {
+        if(i < KNOB_LAST) {
             knob[i].Init(seed.adc.GetPtr(i), AudioCallbackRate()); 
         } else {
-            cv[i-2].InitBipolarCv(seed.adc.GetPtr(i), AudioCallbackRate()); 
+            cv[i-KNOB_LAST].InitBipolarCv(seed.adc.GetPtr(i), AudioCallbackRate()); 
         } 
     } 
 
-    
-
-    DacHandle::Config cfg;
-    cfg.bitdepth   = DacHandle::BitDepth::BITS_12;
-    cfg.buff_state = DacHandle::BufferState::ENABLED;
-    cfg.mode       = DacHandle::Mode::POLLING;
-    cfg.chn        = DacHandle::Channel::BOTH;
-    seed.dac.Init(cfg);
-
-    int direct_led_pins[10] = {PIN_LED1,PIN_LED2,PIN_LED3,PIN_LED4,PIN_LED5,PIN_LED6,PIN_LED7,PIN_LED8,PIN_LED9,PIN_LED10};
-    for(size_t i = 0; i < DIRECT_LEDS_LAST; i++)
+    // RGB LEDs
+    for(size_t i = 0; i < RGB_LED_LAST; i++)
     {
-        green_direct_leds[i].Init(seed.GetPin(direct_led_pins[i]),true);
+        dsy_gpio_pin r = seed.GetPin(ledr_pin[i]);
+        dsy_gpio_pin g = seed.GetPin(ledg_pin[i]);
+        dsy_gpio_pin b = seed.GetPin(ledb_pin[i]);
+        rgb[i].Init(r, g, b, true);
     }
-
-    gate_out_1.mode = DSY_GPIO_MODE_OUTPUT_PP;
-    gate_out_1.pull = DSY_GPIO_NOPULL;
-    gate_out_1.pin  = seed.GetPin(PIN_GATE_OUT);
-    dsy_gpio_init(&gate_out_1);
-
-    encoder.Init(seed.GetPin(PIN_ENC_A_EXT),
-                 seed.GetPin(PIN_ENC_B_EXT),
-                 seed.GetPin(PIN_ENC_CLICK_EXT));
-
-    // OLED
-    OledDisplay<SSD130x4WireSpi128x64Driver>::Config display_config;
-
-    display_config.driver_config.transport_config.pin_config.dc
-        = seed.GetPin(PIN_OLED_CMD_EXT);
-    display_config.driver_config.transport_config.pin_config.reset
-        = {DSY_GPIOX, 0}; // Not a real pin...
-
-    display.Init(display_config);
-
-    screen_update_period_ = 17; // roughly 60Hz
-    screen_update_last_   = seed.system.GetNow();
 
         
 }
@@ -192,11 +147,6 @@ void DaisyHank::StopAdc()
     seed.adc.Stop();
 }
 
-/** Turns on the built-in 12-bit DAC on the Daisy Seed */
-void DaisyHank::StartDac()
-{
-    //dsy_dac_start(DSY_DAC_CHN_BOTH);
-}
 
 void DaisyHank::ProcessAnalogControls()
 {
@@ -224,7 +174,6 @@ bool DaisyHank::TrigIn1()
 void DaisyHank::ProcessDigitalControls()
 {
     s1.Debounce();
-    encoder.Debounce();
 }
 
 
@@ -261,14 +210,9 @@ void DaisyHank::SetHidUpdateRates()
     }
 }
 
-void DaisyHank::SetCvOut1(uint16_t val)
+void DaisyHank::SetLed(size_t idx, float red, float green, float blue)
 {
-    seed.dac.WriteValue(DacHandle::Channel::ONE, val);
-}
-
-void DaisyHank::SetCvOut2(uint16_t val)
-{
-    seed.dac.WriteValue(DacHandle::Channel::TWO, val);
+    rgb[idx].Set(red, green, blue);
 }
 
 
@@ -276,9 +220,9 @@ void DaisyHank::SetCvOut2(uint16_t val)
 void DaisyHank::ClearLeds()
 {
     
-    for(size_t i = 0; i < DIRECT_LEDS_LAST; i++)
+    for(size_t i = 0; i < RGB_LED_LAST; i++)
     {
-        SetGreenDirectLeds(static_cast<DirectLeds>(i), 0.0f);
+        rgb[i].Set(0, 0, 0);
     }
     
 }
@@ -286,17 +230,13 @@ void DaisyHank::ClearLeds()
 void DaisyHank::UpdateLeds()
 {
     
-    for(size_t i = 0; i < DIRECT_LEDS_LAST; i++)
+    for(size_t i = 0; i < RGB_LED_LAST; i++)
     {
-        green_direct_leds[i].Update();
+        rgb[i].Update();
     }
 }
 
 
-void DaisyHank::SetGreenDirectLeds(DirectLeds idx, float bright)
-{
-    green_direct_leds[idx].Set(bright);
-}
 
 
 
--- hank/daisy_hank.h
+++ hank/daisy_hank.h
@@ -20,14 +20,15 @@ class DaisyHank
     
     enum 
     {
-        KNOB_0,   /**< */
         KNOB_1,   /**< */
+        KNOB_2,   /**< */
+        KNOB_3,   /**< */
+        KNOB_4,   /**< */
         KNOB_LAST /**< */
     };
 
     enum 
     {
-        CV_0,   /**< */
         CV_1,   /**< */
         CV_LAST /**< */
     };
@@ -35,66 +36,33 @@ class DaisyHank
 
     enum 
     {
-        LED_0,
-        LED_1,
-        LED_2,
-        LED_3,
-        LED_4,
-        LED_5,
-        LED_6,
-        LED_7,
-        LED_8 /*Led1 R*/,
-        LED_9 /*Led1 G*/,
-        LED_10 /*Led1 B*/,
-        LED_11 /*Led2 R*/,
-        LED_12 /*Led2 G*/,
-        LED_13 /*Led2 B*/,
-        LED_14 /*Led3 R*/,
-        LED_15 /*Led3 G*/,
-        LED_16 /*Led3 B*/,
-        LED_17 /*Led4 R*/,
-        LED_18 /*Led4 G*/,
-        LED_19 /*Led4 B*/,
-        LED_20, 
-        LED_21, 
+        LED_1 /*Led1 R*/,
+        LED_2 /*Led1 G*/,
+        LED_3 /*Led1 B*/,
+        LED_4 /*Led2 R*/,
+        LED_5 /*Led2 G*/,
+        LED_6 /*Led2 B*/,
+        LED_7 /*Led3 R*/,
+        LED_8 /*Led3 G*/,
+        LED_9 /*Led3 B*/,
+        LED_10 /*Led4 R*/,
+        LED_11 /*Led4 G*/,
+        LED_12 /*Led4 B*/,
         LED_LAST
     };
 
-   
-  enum {
-      LED_KNOB_1,
-      LED_KNOB_2,
-      LED_KNOB_3,
-      LED_KNOB_4,
-      LED_KNOB_5,
-      LED_KNOB_6,
-      LED_KNOB_7,
-      LED_KNOB_8,
-      LED_S1,
-      LED_S2,
-      LED_S3,
-      LED_S4,
-      LED_S5,
-      LED_S6,
-      LED_S7,
-      LED_S8
-  };
-
-enum DirectLeds
+    enum Rgbs
     {
-        GREEN_D_LED_1,    /**< & */
-        GREEN_D_LED_2,    /**< & */
-        GREEN_D_LED_3,    /**< & */
-        GREEN_D_LED_4,    /**< & */
-        GREEN_D_LED_5,    /**< & */
-        GREEN_D_LED_6,
-        GREEN_D_LED_7,
-        GREEN_D_LED_8,
-        GREEN_D_LED_9,
-        GREEN_D_LED_10,
-        DIRECT_LEDS_LAST
+        RGB_LED_1,   /**< */
+        RGB_LED_2,   /**< */
+        RGB_LED_3,   /**< */
+        RGB_LED_4,   /**< */
+        RGB_LED_LAST,   /**< */ /**< */
     };
-  
+
+
+
+ 
 
     
 
@@ -206,7 +174,8 @@ enum DirectLeds
         return Random::GetFloat(min, max);
     }
 
-      
+    /** Set an LED (idx < 4) to a color */
+    void SetLed(size_t idx, float red, float green, float blue);  
 
     /**
   General delay _\param del Delay time in ms.
@@ -220,13 +189,6 @@ enum DirectLeds
     void UpdateLeds();
 
 
-    /**
-       Set Green Direct LED
-       \param idx Led Index
-       \param bright Brightness
-     */
-    void SetGreenDirectLeds(DirectLeds idx, float bright);
-
     float CVKnobCombo(float CV_Val,float Pot_Val);
 
     
@@ -235,14 +197,11 @@ enum DirectLeds
     AnalogControl   knob[KNOB_LAST]; /**< Array of AnalogControls */
     AnalogControl   cv[CV_LAST]; /**< Array of AnalogControls */
     GateIn        gate_in1;
-    dsy_gpio      gate_out_1;
-    Led    green_direct_leds[10]; /**< & */
-    Switch3       sw;
+    RgbLed        rgb[RGB_LED_LAST]; /**< & */
     Switch        s1;
-    OledDisplay<SSD130x4WireSpi128x64Driver> display;
-    Encoder         encoder;              /**< Encoder object */
 
-    uint32_t screen_update_last_, screen_update_period_;
+
+
     
 
   private:
